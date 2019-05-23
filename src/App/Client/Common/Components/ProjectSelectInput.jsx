6e0693d0c164b5259a1491e08cb9c289cc1d385a
Ervinne Sodusta <ervinne.sodusta@nuworks.ph>
2019-05-23 16:46:27 +0800

    Fix projects and tasks not updating

--- src/App/Client/Common/Components/ProjectSelectInput.jsx
+++ src/App/Client/Common/Components/ProjectSelectInput.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import Select from 'react-select';
 
-const ProjectSelectInput = ({ selectedProject, projects, name = 'project' }) => (
+const ProjectSelectInput = ({ selectedProject, projects, onProjectSelected, name = 'project' }) => (
     <Select 
         name={ name } 
         isSearchable={ true }
         getOptionValue={ project => project.id }
         getOptionLabel={ project => project.name }
         value={ selectedProject }
+        onChange={ onProjectSelected }
         options={ projects } />
 );
 
--- src/App/Client/Common/Components/TaskSelectInput.jsx
+++ src/App/Client/Common/Components/TaskSelectInput.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import Select from 'react-select';
 
-const TaskSelectInput = ({ selectedTask, tasks, name = "task" }) => (
+const TaskSelectInput = ({ selectedTask, tasks, onTaskSelected, name = "task" }) => (
     <Select 
         name={ name } 
         isSearchable={ true }
         getOptionValue={ task => task.id }
         getOptionLabel={ task => task.name }
         value={ selectedTask }
+        onChange={ onTaskSelected }
         options={ tasks } />
 );
 
