2c4d3a8065ef43369156c5340f30bcf47e387075
Ervinne Sodusta <ervinne.sodusta@nuworks.ph>
2019-05-24 10:34:24 +0800

    Enable straight tracking number generation

--- src/Domain/Services/Documents/TrackingNumberGenerationStrategies.js
+++ src/Domain/Services/Documents/TrackingNumberGenerationStrategies.js
@@ -2,13 +2,39 @@
 import moment from 'moment';
 import TrackingNumberGenerationFailedError from './Errors/TrackingNumberGenerationFailedError';
 
-export const generateDailyBased = ({ module, code, lastNumberUsed, endingNumber }) => {
-    const date = moment().format('YYYYMMDD');
+/**
+ *  Generates something like:
+ *      MC-20190101-000001
+ *  Where:
+ *      MC          = the module code
+ *      20190101    = date today in YYYYMMDD format
+ *      000001      = computed
+ */
+export const generateDailySequence = ({ module, code, lastNumberUsed, endingNumber }) => {    
     lastNumberUsed++;
     validateNumberWithinLimit(module, lastNumberUsed, endingNumber);
+
+    const date = moment().format('YYYYMMDD');
     return {
         lastNumberUsed,
         trackingNumber: `${code}-${date}-${generatePaddedNumber(lastNumberUsed, endingNumber)}`
+    };
+};
+
+/**
+ *  Generates something like:
+ *      MC-00000001
+ *  Where:
+ *      MC          = the module code
+ *      000001      = computed
+ */
+export const generateStraightSequence = ({ module, code, lastNumberUsed, endingNumber }) => {
+    lastNumberUsed++;
+    validateNumberWithinLimit(module, lastNumberUsed, endingNumber);
+
+    return {
+        lastNumberUsed,
+        trackingNumber: `${code}-${generatePaddedNumber(lastNumberUsed, endingNumber)}`
     };
 };
 
--- src/Domain/Services/Documents/TrackingNumberGenerationStrategies.test.js
+++ src/Domain/Services/Documents/TrackingNumberGenerationStrategies.test.js
@@ -1,5 +1,8 @@
 
-import { generateDailyBased } from './TrackingNumberGenerationStrategies';
+import { 
+    generateDailySequence,
+    generateStraightSequence,
+} from './TrackingNumberGenerationStrategies';
 
 import TrackingNumberGenerationFailedError from './Errors/TrackingNumberGenerationFailedError';
 
@@ -17,26 +20,68 @@ afterAll(() => {
     dateNowSpy.mockRestore();
 });
 
-describe("Tracking number daily based generation strategy", () => {
+describe("Tracking number daily sequence generation strategy", () => {
     test("it can generate tracking numbers based on date today", () => {        
         const tnSetup1 = { module: 'test', code: 'T', lastNumberUsed: 0, endingNumber: 99999 };
-        expect(generateDailyBased(tnSetup1))
+        expect(generateDailySequence(tnSetup1))
             .toEqual({ 
                 lastNumberUsed: 1,
                 trackingNumber: `T-20190101-00001`
             });
 
         const tnSetup2 = { module: 'test', code: 'T', lastNumberUsed: 500, endingNumber: 99999 };
-        expect(generateDailyBased(tnSetup2))
+        expect(generateDailySequence(tnSetup2))
             .toEqual({ 
                 lastNumberUsed: 501,
                 trackingNumber: `T-20190101-00501`
             });
     });
 
-    test("it fails to generate if no more tracking number is available", () => {        
-        const tnSetup1 = { module: 'test', code: 'T', lastNumberUsed: 99999, endingNumber: 99999 };        
-        expect(() => generateDailyBased(tnSetup1))
-            .toThrow(TrackingNumberGenerationFailedError.createFromNumberLimitReached('test'));
+    test("it fails to generate if no more tracking number limit is reached", () => {
+        expectGeneratorToThrowLimitReachedWhenRan(generateDailySequence);
     });
 });
+
+describe("Tracking number straight generation strategy", () => {
+    test("it can generate tracking numbers", () => {        
+        const tnSetup1 = { module: 'test', code: 'T', lastNumberUsed: 0, endingNumber: 99999 };
+        expect(generateStraightSequence(tnSetup1))
+            .toEqual({ 
+                lastNumberUsed: 1,
+                trackingNumber: `T-00001`
+            });
+
+        const tnSetup2 = { module: 'test', code: 'T', lastNumberUsed: 500, endingNumber: 99999 };
+        expect(generateStraightSequence(tnSetup2))
+            .toEqual({ 
+                lastNumberUsed: 501,
+                trackingNumber: `T-00501`
+            });
+
+        const tnSetup3 = { module: 'test', code: 'T', lastNumberUsed: 0, endingNumber: 10 };
+        expect(generateStraightSequence(tnSetup3))
+            .toEqual({ 
+                lastNumberUsed: 1,
+                trackingNumber: `T-01`
+            });
+    });
+
+    test("it fails to generate if no more tracking number limit is reached", () => {
+        expectGeneratorToThrowLimitReachedWhenRan(generateStraightSequence);
+    });
+});
+
+const expectGeneratorToThrowLimitReachedWhenRan = generator => {
+    const module = 'test';
+    const tnExpiredSetup = { code: 'T', lastNumberUsed: 99999, endingNumber: 99999, module };        
+    expect(() => generator(tnExpiredSetup))
+        .toThrow(TrackingNumberGenerationFailedError.createFromNumberLimitReached('test'));
+
+    const tnBadSetup1 = { code: 'T', lastNumberUsed: 120, endingNumber: 0, module };        
+    expect(() => generator(tnBadSetup1))
+        .toThrow(TrackingNumberGenerationFailedError.createFromNumberLimitReached('test'));
+
+    const tnBadSetup2 = { code: 'T', lastNumberUsed: 0, endingNumber: -50, module };        
+    expect(() => generator(tnBadSetup2))
+        .toThrow(TrackingNumberGenerationFailedError.createFromNumberLimitReached('test'));
+};
